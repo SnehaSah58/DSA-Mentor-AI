1398ddec5ec130786ab06a707acf678da4a41661
SnehaSah <snehasah0307@gmail.com>
2026-05-08 15:50:38 +0530

    Completed CRUD Functionality

--- dsa.js
+++ dsa.js
@@ -1,3 +1,4 @@
+const methodOverride = require("method-override");
 const Question = require("./models/Question");
 const mongoose = require("mongoose");
 const express = require ("express");
@@ -8,25 +9,53 @@ mongoose.connect("mongodb://127.0.0.1:27017/dsaMentor")
 .catch((err) => console.log(err));
 
 app.set("view engine" , "ejs");
-app.use(express.urlencoded({extended: true}));   //for read data
 
+app.use(express.urlencoded({extended: true}));   //for read data
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.render("home");
 });
 
-app.get("/add-question",(req,res)=>{
+app.get("/add-Question",(req,res)=>{
     res.render("addQuestion");
 });
 
-app.post("/add-question",async(req,res)=>{
+
+app.post("/add-Question",async(req,res)=>{
     const {title , difficulty, topic, description } = req.body;
     const newQuestion = new Question({
         title, difficulty, topic, description
     });
     await newQuestion.save();                 // mongodb save horha
     res.send("Question Added Successfully");
+});
+
+
+app.get("/Questions",async(req,res)=>{
+    const allQuestions = await Question.find();
+    res.render("questions",{allQuestions});
+});
+
+app.get("/edit/:id", async(req,res)=>{
+    const foundQuestion = await Question.findById(req.params.id);
+    res.render("edit",{foundQuestion    
+    });
+});
+
+app.delete("/delete/:id", async (req,res)=>{
+    await Question.findByIdAndDelete(req.params.id);
+    res.redirect("/questions");
+
+});
+
+app.put("/edit/:id", async(req,res)=>{
+    await Question.findByIdAndUpdate(
+        req.params.id,
+        req.body
+    );
+    res.redirect("/questions");
 });
 
 app.listen(3000,()=>{
